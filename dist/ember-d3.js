1515ed341091ceea3fb2e54d9c8a2d8de7ff3bf0
Antoine Moser <moser.antoine@gmail.com>
2014-06-29 11:53:26 +0200

    New version with bar-chart

--- dist/ember-d3.js
+++ dist/ember-d3.js
@@ -6,12 +6,127 @@
 (function() {
 
 Ember.Chart = Ember.Namespace.create();
-Ember.Chart.VERSION = '0.0.1';
+Ember.Chart.VERSION = '0.0.2';
 
 Ember.libraries.register('ember-d3', Ember.Chart.VERSION);
 
 })();
 
+(function() {
+
+  Ember.Chart.BarChartComponent = Ember.Component.extend({
+    tagName: 'svg',
+    margin: {
+      top: 35,
+      right: 35,
+      bottom: 35,
+      left: 35
+    },
+
+    didInsertElement: function() {
+      this.draw();
+    },
+
+    draw: function() {
+      var width = this.get('width') - this.margin.left - this.margin.right,
+          height = this.get('height') - this.margin.top - this.margin.bottom,
+          data = this.get('data');
+
+      var viewXAxis = this.get('xAxis');
+      var viewYAxis = this.get('yAxis');
+      var formatX = this.get('formatXAxis');
+
+      this.height = height;
+      this.width = width;
+
+      var x = this.createX(data);
+      var y = this.createY(data);
+
+      /* Create chart */
+      this.chart = d3.select('#'+this.get('elementId'))
+        .attr('id', 'bar-chart')
+        .attr('width', width + this.margin.left + this.margin.right)
+        .attr('height', height + this.margin.top + this.margin.bottom)
+        .append('svg:g')
+        .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+
+      this.chart.append('svg:g')
+        .attr('class', 'rects');
+
+      this.chart.select('.rects').selectAll('rect')
+        .data(data)
+        .enter().append('rect')
+        .attr('class', 'bar')
+        .attr('x', function(d) { return x(d.keyD); })
+        .attr('y', function(d) { return y(d.valD); })
+        .attr('width', x.rangeBand())
+        .attr('height', function(d) { return height - y(d.valD); })
+        .attr('fill', function(d) {
+          if(d.colorD === undefined)
+            return '#428bca';
+          else
+            return d.colorD;
+        });
+
+      if(viewXAxis) {
+        this.drawXAxis(x, data, formatX);
+      }
+
+      if(viewYAxis) {
+        var legendY = this.get("legendY");
+        this.drawYAxis(y, legendY);
+      }
+    },
+
+    createX: function(data, type) {
+      return d3.scale.ordinal().rangeRoundBands([0, this.width], 0.1).domain(data.map(function(d) { return d.keyD; }));
+    },
+
+    createY: function(data) {
+      return d3.scale.linear().range([this.height, 0]).domain([0, d3.max(data, function(d) { return d.valD; }) + 10]);
+    },
+
+    drawXAxis: function(x, data, formatXAxis, legendX, height) {
+      var xAxis = d3.svg.axis().scale(x).orient("bottom");
+
+      if(formatXAxis === 'date') {
+        xAxis.ticks(d3.time.days, 1).tickFormat(this.timeFormatter);
+      } else {
+        xAxis.ticks(data.length);
+      }
+
+      this.chart.append('svg:g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0,'+this.height+')')
+        .call(xAxis);
+
+      return xAxis;
+    },
+
+    drawYAxis: function(y, legendY) {
+      var yAxis = d3.svg.axis().scale(y).orient("left");
+
+      this.chart.append('svg:g')
+        .attr('class', 'y axis')
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text(legendY);
+
+      return yAxis;
+    },
+
+    timeFormatter: function(date) {
+      return moment(date).format('DD/MM');
+    }
+  });
+
+  Ember.Handlebars.helper('bar-chart', Ember.Chart.BarChartComponent);
+
+}());
 (function() {
 
 Ember.Chart.LineChartComponent = Ember.Component.extend({
@@ -28,11 +143,66 @@ Ember.Chart.LineChartComponent = Ember.Component.extend({
       this.draw();
     },
 
+    draw_old: function() {
+      var width = this.get('width') - this.margin.left - this.margin.right,
+        height = this.get('height') - this.margin.top - this.margin.bottom,
+        data = this.get('data');
+
+    var formatX = this.get('formatX');
+    var color = this.get('color');
+    var tooltip = this.get('tooltip');
+    var x = null;
+
+    var y = d3.scale.linear().range([height, 0]).domain([0, d3.max(data, function(d) { return d.valD; })+10]);
+    var viewXAxis = this.get('xAxis');
+    var viewYAxis = this.get('yAxis');
+    var line = d3.svg.line().y(function(d) { return y(d.valD); });
+
+    if(color === undefined) {
+      color = "#428bca";
+    }
+
+    var formatData = [];
+
+      formatData = data;
+      x = d3.scale.linear().range([0, width]).domain([0, formatData.length-1]);
+      line.x(function(d) { return x(d.keyD); });
+
+    var chart = d3.select('#'+this.get('elementId'))
+      .attr('id', 'chart')
+      .attr('width', width + this.margin.left + this.margin.right)
+      .attr('height', height + this.margin.top + this.margin.bottom)
+      .append('svg:g')
+      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+
+    chart.append('svg:clipPath')
+      .attr('id', 'clip')
+      .append('svg:rect')
+      .attr('width', width + this.margin.left + this.margin.right)
+      .attr('height', height + this.margin.top + this.margin.bottom);
+
+    chart.append('svg:path')
+      .attr('class', 'line')
+      .attr('clip-path', 'url(#clip)')
+      .attr('stroke', color)
+      .attr('d', line(formatData));
+
+    if(viewXAxis) {
+      var xAxis = d3.svg.axis().scale(x).orient("bottom");
+
+      chart.append('svg:g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(xAxis);
+    }
+    },
+
     draw: function() {
       var width = this.get('width') - this.margin.left - this.margin.right,
           height = this.get('height') - this.margin.top - this.margin.bottom,
           data = this.get('data');
 
+this.width = width;
       this.height = height;
 
       var formatX = this.get('formatXAxis');
@@ -46,7 +216,6 @@ Ember.Chart.LineChartComponent = Ember.Component.extend({
       var x = this.createX(formatedData, formatX);
       var y = this.createY(formatedData);
 
-    
       if(color === undefined) {
         color = "#428bca";
       }
@@ -71,7 +240,6 @@ Ember.Chart.LineChartComponent = Ember.Component.extend({
         .attr('class', 'line')
         .attr('clip-path', 'url(#clip)')
         .attr('stroke', color)
-        .attr("class", "line")
         .attr('d', line(formatedData));
 
       var points = this.drawPoints(formatedData, x, y);
@@ -95,7 +263,7 @@ Ember.Chart.LineChartComponent = Ember.Component.extend({
       if(type === 'date') {
         x = d3.scale.ordinal().rangePoints([0, this.width]).domain(data.map(function(d) { return d.keyD; }));
       } else {
-        x = d3.scale.linear().range([0, this.width]).domain([0, data.length-1]);
+        x = d3.scale.linear().range([0, this.width]).domain([0, data.length - 1]);
       }
 
       return x;
@@ -186,6 +354,8 @@ Ember.Chart.LineChartComponent = Ember.Component.extend({
 
           formatedData.push(day);
         });
+      } else {
+        formatedData = data;
       }
 
       return formatedData;
