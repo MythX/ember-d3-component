42c5bd403cd459f8e8b7a6b78a57d78e1ce17da6
MythX <moser.antoine@gmail.com>
2014-07-08 10:57:02 +0200

    Version 0.0.4
    Changelog :
     - multicolor bar chart
     - animation for line-chart

--- dist/ember-d3.js
+++ dist/ember-d3.js
@@ -1,13 +1,13 @@
 // ==========================================================================
 // Project:   Ember D3 Component
-// Version    v0.0.3
+// Version    v0.0.4
 // Copyright: © 2014 Antoine Moser
 // License:   MIT (see LICENSE)
 // ==========================================================================
 (function() {
 
 Ember.Chart = Ember.Namespace.create();
-Ember.Chart.VERSION = '0.0.3';
+Ember.Chart.VERSION = '0.0.4';
 
 Ember.libraries.register('ember-d3', Ember.Chart.VERSION);
 
